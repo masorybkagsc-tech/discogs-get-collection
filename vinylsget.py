c27a10d3a6982bcf1c9e25e5ea4a6cc07b2c99a1
masorybkagsc-tech <masorybkagsc@gmail.com>
2026-01-02 19:20:44 +0100

    Update vinylsget.py

--- vinylsget.py
+++ vinylsget.py
@@ -3,9 +3,9 @@
 import discogs_client
 import csv
 
-userToken = ""
+userToken = "YGTofvyfMlcKIMVsmgNdxKofzHnXUhPBcSCrGZxB"
 
-if userToken == "":
+if userToken == "YGTofvyfMlcKIMVsmgNdxKofzHnXUhPBcSCrGZxB":
     exit("Whoops, you need to set your user token.")
 
 d = discogs_client.Client("CollectionGenreClassifier/0.1 +http://github.com/victorloux",
